b8f3ceb20d344863f889c4057aed9caba3af9ee3
Remi Gau <remi_gau@hotmail.com>
2021-10-09 18:53:27 +0200

    remove slice timing info from dataset: uncertain if the current info was accurate

--- code/convert_spm_multimodal_ds.m
+++ code/convert_spm_multimodal_ds.m
@@ -282,32 +282,6 @@ function create_readme(output_dir)
   end
   fclose(fid);
 
-  % The basic paradigm involves randomised presentation of at least 86 faces and 86 scrambled faces
-  % (Figure 37.1), based on Phase 1 of a previous study by Henson et al (2003). The scrambled faces
-  % were created by 2D Fourier transformation, random phase permutation, inverse transformation
-  % and outline-masking of each face. Thus faces and scrambled faces are closely matched for low-level
-  % visual properties such as spatial frequency content. Half the faces were famous, but this factor is
-  % collapsed in the current analyses. Each face required a four-way, left-right symmetry judgment
-  % (mean RTs over a second; judgments roughly orthogonal to conditions; reasons for this task are
-  % explained in Henson et al, 2003). The subject was instructed not to blink while the fixation cross
-  % was present on the screen.
-
-  % The T1-weighted structural MRI of a young male was acquired on a 1.5T Siemens Sonata via an
-  % MDEFT sequence
-
-  % The fMRI data were acquired using a gradient-echo EPI sequence on a 3T Siemens TIM Trio,
-  % with 32, 3mm slices (skip 0.75mm)
-
-  % The EEG data were acquired on a 128-channel ActiveTwo system, sampled at 2048 Hz, plus
-  % electrodes on left earlobe, right earlobe, and two bipolar channels to measure HEOG and VEOG.
-  % The 128 scalp channels are named: 32 A (Back), 32 B (Right), 32 C (Front) and 32 D (Left).
-
-  % The MEG data were acquired on a 275 channel CTF/VSM system, using second-order axial
-  % gradiometers and synthetic third gradient for denoising and sampled at 480 Hz. There are actually
-  % 274 MEG channels in this dataset since the system it was recorded on had one faulty sensor. Two
-  % runs (sessions) of the protocol have been saved in two CTF datasets (each one is a directory with
-  % multiple files)
-
 end
 
 function create_changelog(output_dir)
@@ -345,14 +319,10 @@ end
 
 function create_bold_json(output_dir, task_name, opt, func)
 
-  acquisition_time = func.repetition_time - func.repetition_time / func.nb_slices;
-  slice_timing = linspace(acquisition_time, 0, func.nb_slices);
-
   task = struct('Manufacturer', func.manufacturer, ...
                 'ManufacturersModelName', func.model, ...
                 'MagneticFieldStrength', func.field_strength, ...
                 'RepetitionTime', func.repetition_time, ...
-                'SliceTiming', slice_timing, ...
                 'NumberOfVolumesDiscardedByUser', func.discarded_volumes, ...
                 'TaskName', task_name, ...
                 'TaskDescription', '???');
